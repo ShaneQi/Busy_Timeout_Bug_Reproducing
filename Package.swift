0d06ddfb4dfbe34ca5889e17441fa051f542f6eb
ShaneQi <qizengtai@gmail.com>
2016-10-11 13:29:38 -0500

    Updated Perfect-SQLite version.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 		name: "Busy_Timeout_Bug_Reproducing",
 		dependencies: [
-		.Package(url:"https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 2, minor: 0),
+		.Package(url:"https://github.com/PerfectlySoft/Perfect-SQLite.git", Version(2,0,1)),
 		]
 		)
 
